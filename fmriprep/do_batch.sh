b0cdb488a81001f4ad3a15b98d8fbe01683d1723
xscapintime <louiesxscape@gmail.com>
2022-07-25 15:04:59 +0100

    ooops wrong order

--- fmriprep/do_batch.sh
+++ fmriprep/do_batch.sh
@@ -2,7 +2,7 @@ for list in *liyang_*.txt
 do
 	n=`echo $list | cut -d _ -f 3 | sed "s/.txt//g"`
 	nl=`cat $list | wc -l`
-	qsub STEP_4_fMRIprep_batch.sh -v list=${list} -N Liyang_fmriprep_Parallel_${n} -t 1-${nl}
+	qsub -v list=${list} -N Liyang_fmriprep_Parallel_${n} -t 1-${nl} STEP_4_fMRIprep_batch.sh
 	# qsub STEP_4_fMRIprep_batch.sh -v list=${list} -N Liyang_fmriprep_Parallel_${n} -t 1-${nl} -hold_jid ${previous job name} ## if still not enough storage
 done
 
