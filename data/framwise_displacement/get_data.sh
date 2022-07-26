4f7832f49f7f09071b49532332963a20540e03ad
xscapintime <louiesxscape@gmail.com>
2022-07-26 15:58:40 +0100

    get fd and timeseries from the original folders

--- data/framwise_displacement/get_data.sh
+++ data/framwise_displacement/get_data.sh
@@ -1,1 +1,4 @@
-rsync -arv 'k21188249@login1.nan.kcl.ac.uk:/data/project/BIPP/laila/AP_8yo_fmri_task/liyang_files_for_FC/*_framwise_displacement.tsv' .
+# rsync -arv 'k21188249@login1.nan.kcl.ac.uk:/data/project/BIPP/laila/AP_8yo_fmri_task/liyang_files_for_FC/*_framwise_displacement.tsv' .
+
+## 
+rsync -arv --exclude 'sub-EPA*/*' 'k21188249@login1.nan.kcl.ac.uk:/data/project/BIPP/laila/AP_8yo_fmri_task/derivatives/fmriprep/sub-*/func/*_framwise_displacement.tsv' .
--- data/timeseries_byglasserROIs/get_data.sh
+++ data/timeseries_byglasserROIs/get_data.sh
@@ -1,1 +1,4 @@
-rsync -arv 'k21188249@login1.nan.kcl.ac.uk:/data/project/BIPP/laila/AP_8yo_fmri_task/liyang_files_for_FC/*.txt' .
+# rsync -arv 'k21188249@login1.nan.kcl.ac.uk:/data/project/BIPP/laila/AP_8yo_fmri_task/liyang_files_for_FC/*.txt' .
+
+## 
+rsync -arv --exclude 'sub-EAP*/*' 'k21188249@login1.nan.kcl.ac.uk:/data/project/BIPP/laila/AP_8yo_fmri_task/derivatives/freesurfer/sub-*/parcellation/*_timeseries_sept29.txt' .
