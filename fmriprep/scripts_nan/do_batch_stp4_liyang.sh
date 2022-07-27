b5ca997d34fbb0c6d0568c89e2d1d0f68536387a
xscapintime <louiesxscape@gmail.com>
2022-07-27 18:02:21 +0100

    ooops

--- fmriprep/scripts_nan/do_batch_stp4_liyang.sh
+++ fmriprep/scripts_nan/do_batch_stp4_liyang.sh
@@ -7,14 +7,14 @@ do
 	if [ $n == 1 ]
 	then
 		echo fmriprep_Parallel_${n} submitted
-		echo $bn $nl
+		echo $bn subjet: $nl
 		qsub -v list=${list} -N fmriprep_Parallel_${n} -t 1-${nl} STEP_4_fMRIprep_run_forLiyang_batch.sh
 		sleep 0.5
 	else
 		prej=`expr $n - 1`
 		echo fmriprep_Parallel_${n} will be submitted after fmriprep_Parallel_${prej} is finised
-		echo $bn $nl
-		qsub -v list=${list} -N Liyang_fmriprep_Parallel_${n} -t 1-${nl} -hold_jid fmriprep_Parallel_${prej} STEP_4_fMRIprep_run_forLiyang_batch.sh ## if still not enough storage
+		echo $bn subject: $nl
+		qsub -v list=${list} -N fmriprep_Parallel_${n} -t 1-${nl} -hold_jid fmriprep_Parallel_${prej} STEP_4_fMRIprep_run_forLiyang_batch.sh ## if still not enough storage
 		sleep 0.5
 
 	fi		
