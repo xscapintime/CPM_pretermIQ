32bb67c2b793c6db955cc513b54ebea5886c548d
xscapintime <louiesxscape@gmail.com>
2022-08-11 16:44:13 +0100

    run each subj individualy

---
+++ fmriprep/scripts_nan/do_batch_stp4_liyang.v2.sh
@@ -0,0 +1,25 @@
+awk '{print NR  " " $s}' /data/project/BIPP/laila/AP_8yo_fmri_task/lists/list_liyang_new.txt > list_tmp
+
+cat list_tmp | while read id
+do
+
+	n=`echo $id | cut -d " " -f 1`
+	sub=`echo $id | cut -d " " -f 2`
+
+	if [ $n == 1 ]
+	then
+		echo fmriprep.${n} for ${sub} submitted
+		#echo $sub
+		qsub -v subj=${sub} -N fmriprep.${n} STEP_4_fMRIprep_run_forLiyang_batch.v2.sh
+		sleep 0.5
+	else
+		prej=`expr $n - 1`
+		echo fmriprep.${n} for ${sub} submitted, will start after fmriprep.${prej} is finised
+		#echo $sub
+		qsub -v subj=${sub} -N fmriprep.${n} -hold_jid fmriprep.${prej} STEP_4_fMRIprep_run_forLiyang_batch.v2.sh ## if still not enough storage
+		sleep 0.5
+
+	fi		
+done
+
+rm list_tmp
