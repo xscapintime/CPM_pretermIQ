fa5a7d1cb901ae60d5087c71f0a2a210ce80adbb
xscapintime <louiesxscape@gmail.com>
2022-10-20 04:10:42 +0800

    use 6 vars pca, save all_mask for later

--- cpm_py/cpm_plots.py
+++ cpm_py/cpm_plots.py
@@ -6,7 +6,7 @@ import scipy as sp
 from scipy.stats import kstest
 import statistics
 from matplotlib import pyplot as plt
-from cpmFinctions import *
+from cpmFunctions import *
 
 
 ## check FC file names
@@ -18,23 +18,31 @@ file = glob.glob(os.path.join(path,'*.txt'))
 subj_list = [ os.path.split(f)[1].replace('.fc.txt', '') for f in file ]
 subj_list = np.array(subj_list, dtype=str)
 subj_list
+# remove 7 for external validation
+mask = np.ones(len(subj_list), dtype=bool)
+mask[[75,76,77,78,79,80,81]] = False
+subj_list = subj_list[mask]
+len(subj_list)
 
 
 ## beahve data
-all_behav_data = pd.read_csv('../data/id_vars_fin.csv', dtype={'Eprime_ID': str})
+# all_behav_data = pd.read_csv('../data/id_vars_fin.csv', dtype={'Eprime_ID': str})
+all_behav_data = pd.read_csv('../data/vars_8yo_82subj_imputed.csv', index_col=0)
 all_behav_data.dtypes
 
 # filter by AP ID
-all_behav_data = all_behav_data[all_behav_data['AP_ID'].isin(subj_list)]
-all_behav_data = all_behav_data.set_index('AP_ID')
+all_behav_data = all_behav_data[all_behav_data.index.isin(subj_list)]
+# all_behav_data = all_behav_data.set_index('AP_ID')
 
 
 # for modeling, no NA allowed
-behav_data = all_behav_data.iloc[:,6:].dropna()
+# only keep 8yo
+# behav_data = all_behav_data.iloc[:,6:11]#.dropna()
+behav_data = all_behav_data
 print(behav_data.shape)
 
 ## PCs
-all_pca_data = pd.read_csv('../data/var17_imp_pca.csv', index_col=0)
+all_pca_data = pd.read_csv('../data/var6_8yo_sbj82_imp_pca.csv', index_col=0)
 all_pca_data.dtypes
 
 # filter
@@ -63,15 +71,30 @@ for s in range(0,all_fc_data.shape[0]):
 
 
 # compare all the vars and PCs
-plt.figure(figsize = (12,12))
+# plt.figure(figsize = (12,12))
+# p = sns.pairplot(behav_data, kind ='scatter')
+# plt.savefig(os.path.join('dist', 'var17_pairdist.pdf'))
+# plt.close()
+
+# plt.figure(figsize = (5,5))
+# p = sns.pairplot(pca_data.iloc[:,:2], kind ='scatter')
+# plt.savefig(os.path.join('dist', 'pcs2_pairdist.pdf'))
+# plt.close()
+
+# plt.figure(figsize = (12,12))
+# p = sns.pairplot(pca_data, kind ='scatter')
+# plt.savefig(os.path.join('dist', 'pca_pairdist.pdf'))
+# plt.close()
+
+plt.figure(figsize = (6,6))
 p = sns.pairplot(behav_data, kind ='scatter')
-plt.savefig(os.path.join('dist', 'var17_pairdist.pdf'))
+plt.savefig(os.path.join('dist', 'var6_8yo_pairdist.pdf'))
 plt.close()
 
-plt.figure(figsize = (5,5))
-p = sns.pairplot(pca_data.iloc[:,:2], kind ='scatter')
-plt.savefig(os.path.join('dist', 'pcs2_pairdist.pdf'))
-plt.close()
+# plt.figure(figsize = (5,5))
+# p = sns.pairplot(pca_data.iloc[:,:2], kind ='scatter')
+# plt.savefig(os.path.join('dist', 'pcs2_pairdist.pdf'))
+# plt.close()
 
 plt.figure(figsize = (12,12))
 p = sns.pairplot(pca_data, kind ='scatter')
@@ -85,9 +108,9 @@ fd = pd.read_csv('../data/fd_mean_max.txt', sep=' ', header=0)
 fd = fd[fd.index.isin(pca_data.index)]
 
 sp.stats.pearsonr(fd['fd.m'], pca_data['PC1'])
-# (0.1783240798203215, 0.1258458793145855)
+# (0.3105841565689169, 0.006688262332681814)
 sp.stats.pearsonr(fd['fd.m'], pca_data['PC2'])
-# (-0.2585155832221153, 0.02512842358871094)
+# (0.05783213135402286, 0.622124464908075)
 
 for pc in ['PC1', 'PC2'] :
     
@@ -106,88 +129,91 @@ for pc in ['PC1', 'PC2'] :
 
 ## check normality of PCs
 kstest(pca_data['PC1'], 'norm')
-# KstestResult(statistic=0.22012991583245278, pvalue=0.0011335296969197106)
-# pval < 0.05, rejct H0, that PC1 is not normally distributed
+# KstestResult(statistic=0.1660644636021128, pvalue=0.02820521696212752)
+# pval < 0.05, rejct H0, that PC1 is not normally distributed ###
+
+#### pval > 0.05, reject H1, PC1 normal
 
 kstest(pca_data['PC2'], 'norm')
-# KstestResult(statistic=0.20643213219397566, pvalue=0.002792582138901656)
+# KstestResult(statistic=0.1054905957612926, pvalue=0.3496145154997725)
 
 
-## load covariates data
-cova_data = pd.read_csv('../data/pt_sbj75withfc_newvars_imp_fd_pca_merged.csv', index_col=1)
-cova_data = cova_data[['sex', 'age22', 'age4', 'age8', 'Neonatal Sickness', 'IMD Score']]#.dropna() ##
-cova_data.shape
+# ## load covariates data
+# cova_data = pd.read_csv('../data/pt_sbj75withfc_newvars_imp_fd_pca_merged.csv', index_col=1)
+# cova_data = cova_data[['sex', 'age8', 'Neonatal Sickness', 'IMD Score']]#.dropna() ##
+# cova_data.shape
 
 
-##### for NA in covariates ####
-### might remove this step ###
-fc_data = all_fc_data.loc[cova_data.index]
-print(fc_data.shape)
+# ##### for NA in covariates ####
+# ### might remove this step ###
+# fc_data = all_fc_data.loc[cova_data.index]
+# print(fc_data.shape)
 
-pca_data = pca_data.loc[cova_data.index]
-print(pca_data.shape)
-
-
-
-## viz edges
-coords = pd.read_csv("../data/coords/MMP_MNI_374_UCCHILD_coords.txt", index_col=None, header=None, sep=" ")
-print(coords.shape)
-
-# paras for CPM
-cpm_kwargs = {'r_thresh': 0.38, 'corr_type': 'spearman', 'verbose': False} ## use spearman if the distribution is skewed
-# perc_thresh=1 for top edges selection
-# cpm_kwargs = {'p_thresh': 0.01, 'corr_type': 'pearson', 'verbose': False} ## use spearman if the distribution is skewed
-covar = ['sex', 'age22', 'age4', 'age8', 'Neonatal Sickness', 'IMD Score']
+# pca_data = pca_data.loc[cova_data.index]
+# print(pca_data.shape)
 
 
-# plots, would be one of the random cases
-# set k as the number of subjects, Leave-One-Out Cross-Validation (LOOCV)
 
-## make a test fc_data of 50 nodes, 68*67/2
-fc_data_t = fc_data.loc[:,:2277]
+# ## viz edges
+# coords = pd.read_csv("../data/coords/MMP_MNI_374_UCCHILD_coords.txt", index_col=None, header=None, sep=" ")
+# print(coords.shape)
 
-### k as number of subjects, LOOCV
-k = fc_data_t.shape[0]
-corr_type = 'spearman'
+# # paras for CPM
+# cpm_kwargs = {'r_thresh': 0.35, 'corr_type': 'spearman', 'verbose': False} ## use spearman if the distribution is skewed
+# # perc_thresh=1 for top edges selection
+# # cpm_kwargs = {'p_thresh': 0.01, 'corr_type': 'pearson', 'verbose': False} ## use spearman if the distribution is skewed
+# covar = ['sex', 'age8', 'Neonatal Sickness', 'IMD Score']
 
-for behav in pca_data.columns[:2]:
-    print(behav)
 
-    behav_obs_pred, all_masks, corr = cpm_wrapper_seed_part(fc_data_t, pca_data, behav=behav, cova_data=cova_data, covar=covar, k=k, seed=202209, **cpm_kwargs)
-    ## count selected edges
-    print('{:.2f} pos edges passed all folds: '.format(((all_masks['pos'].sum(axis=0)/10) >= 1).sum()))
-    print('{:.2f} neg edges passed all folds: '.format(((all_masks['neg'].sum(axis=0)/10) >= 1).sum()))
+# # plots, would be one of the random cases
+# # set k as the number of subjects, Leave-One-Out Cross-Validation (LOOCV)
+
+# ## make a test fc_data of 50 nodes, 68*67/2
+# fc_data_t = fc_data.loc[:,:2277]
+
+# ### k as number of subjects, LOOCV
+# # k = fc_data_t.shape[0]
+# k = 10
+# corr_type = 'spearman'
+
+# for behav in pca_data.columns[0:1]: #PC1
+#     print(behav)
+
+#     behav_obs_pred, all_masks, corr = cpm_wrapper_seed_part(fc_data, pca_data, behav=behav, cova_data=cova_data, covar=covar, k=k, seed=202209, **cpm_kwargs)
+#     ## count selected edges
+#     print('{:.2f} pos edges passed all folds: '.format(((all_masks['pos'].sum(axis=0)/10) >= 1).sum()))
+#     print('{:.2f} neg edges passed all folds: '.format(((all_masks['neg'].sum(axis=0)/10) >= 1).sum()))
     
-    ## export pred table
-    fn = behav + '_' + corr_type + '_fold_' + str(k) + '_partcorr_pred.csv'
-    behav_obs_pred.to_csv(fn)
+#     ## export pred table
+#     fn = behav + '_' + corr_type + '_fold_' + str(k) + '_partcorr_pred.csv'
+#     behav_obs_pred.to_csv(fn)
 
-    ## plot pred vs obs
-    g = plot_predictions(behav_obs_pred)
-    g.figure.tight_layout()
-    plt.savefig(os.path.join('models', behav + '_' + corr_type + '_fold_' + str(k) + '_partcorr_model.png'))
-    plt.close()
+#     ## plot pred vs obs
+#     g = plot_predictions(behav_obs_pred)
+#     g.figure.tight_layout()
+#     plt.savefig(os.path.join('models', behav + '_' + corr_type + '_fold_' + str(k) + '_partcorr_model.png'))
+#     plt.close()
 
     
-    ## edges
-    for tail in all_masks.keys():
-        edfn = tail + '_' + behav + '_' + corr_type + '_fold_' + str(k)
+#     ## edges
+#     for tail in all_masks.keys():
+#         edfn = tail + '_' + behav + '_' + corr_type + '_fold_' + str(k)
         
-        edge_frac = (all_masks[tail].sum(axis=0))/(all_masks[tail].shape[0])
-        edge_frac_square = sp.spatial.distance.squareform(edge_frac)
+#         edge_frac = (all_masks[tail].sum(axis=0))/(all_masks[tail].shape[0])
+#         edge_frac_square = sp.spatial.distance.squareform(edge_frac)
         
-        # export pos and neg matrices
-        edmat = edfn + '_partcorr_bn.csv'
-        np.savetxt(edmat, edge_frac_square, delimiter=',', fmt='%1.0f')
+#         # export pos and neg matrices
+#         edmat = edfn + '_partcorr_bn.csv'
+#         np.savetxt(edmat, edge_frac_square, delimiter=',', fmt='%1.0f')
     
-        # plot edges
-        # all_masks(pos/neg matrices) is a binary array, k fold * n total edges, in this case is 10,69751
-        if tail == 'pos':
-            color = 'red'
-        else:
-            color = 'blue'
+#         # plot edges
+#         # all_masks(pos/neg matrices) is a binary array, k fold * n total edges, in this case is 10,69751
+#         if tail == 'pos':
+#             color = 'red'
+#         else:
+#             color = 'blue'
         
-        g = plot_consistent_edges(all_masks, tail, thresh = 1, color = color, node_coords = coords)
-        plt.savefig(os.path.join('edges', edfn + '_partcorr_edges.png'))
-        plt.close()
+#         g = plot_consistent_edges(all_masks, tail, thresh = 1, color = color, node_coords = coords)
+#         plt.savefig(os.path.join('edges', edfn + '_partcorr_edges.png'))
+#         plt.close()
     
--- cpm_py/loocv_only_pearson.py
+++ cpm_py/loocv_only_pearson.py
@@ -6,7 +6,7 @@ import scipy as sp
 from scipy.stats import kstest
 import statistics
 from matplotlib import pyplot as plt
-from cpmFinctions import *
+from cpmFunctions import *
 
 
 ## check FC file names
@@ -18,23 +18,30 @@ file = glob.glob(os.path.join(path,'*.txt'))
 subj_list = [ os.path.split(f)[1].replace('.fc.txt', '') for f in file ]
 subj_list = np.array(subj_list, dtype=str)
 subj_list
+# remove 7 for external validation
+mask = np.ones(len(subj_list), dtype=bool)
+mask[[75,76,77,78,79,80,81]] = False
+subj_list = subj_list[mask]
+len(subj_list)
 
 
 ## beahve data
-all_behav_data = pd.read_csv('../data/id_vars_fin.csv', dtype={'Eprime_ID': str})
-all_behav_data.dtypes
+# all_behav_data = pd.read_csv('../data/id_vars_fin.csv', dtype={'Eprime_ID': str})
+# all_behav_data.dtypes
 
-# filter by AP ID
-all_behav_data = all_behav_data[all_behav_data['AP_ID'].isin(subj_list)]
-all_behav_data = all_behav_data.set_index('AP_ID')
+# # filter by AP ID
+# all_behav_data = all_behav_data[all_behav_data['AP_ID'].isin(subj_list)]
+# all_behav_data = all_behav_data.set_index('AP_ID')
 
 
-# for modeling, no NA allowed
-behav_data = all_behav_data.iloc[:,6:].dropna()
-print(behav_data.shape)
+# # for modeling, no NA allowed
+# behav_data = all_behav_data.iloc[:,6:].dropna()
+# print(behav_data.shape)
+
 
 ## PCs
-all_pca_data = pd.read_csv('../data/var17_imp_pca.csv', index_col=0)
+# 8 yo vars only
+all_pca_data = pd.read_csv('../data/var6_8yo_sbj82_imp_pca.csv', index_col=0)
 all_pca_data.dtypes
 
 # filter
@@ -73,7 +80,7 @@ coords = pd.read_csv("../data/coords/MMP_MNI_374_UCCHILD_coords.txt", index_col=
 print(coords.shape)
 
 # paras for CPM
-cpm_kwargs = {'r_thresh': 0.38, 'corr_type': 'pearson', 'verbose': False} ## use spearman if the distribution is skewed
+cpm_kwargs = {'r_thresh': 0.35, 'corr_type': 'pearson', 'verbose': False} ## use spearman if the distribution is skewed
 # perc_thresh=1 for top edges selection
 # cpm_kwargs = {'p_thresh': 0.01, 'corr_type': 'pearson', 'verbose': False} ## use spearman if the distribution is skewed
 # covar = ['sex', 'age22', 'age4', 'age8', 'Neonatal Sickness', 'IMD Score']
@@ -94,30 +101,32 @@ for behav in pca_data.columns[:2]:
 
     behav_obs_pred, all_masks, corr = cpm_wrapper_seed(fc_data, pca_data, behav=behav, k=k, seed=202209, **cpm_kwargs)
     ## count selected edges
-    print('{:.2f} pos edges passed all folds: '.format(((all_masks['pos'].sum(axis=0)/10) >= 1).sum()))
-    print('{:.2f} neg edges passed all folds: '.format(((all_masks['neg'].sum(axis=0)/10) >= 1).sum()))
+    print('{:.2f} pos edges passed the threshold at all folds: '.format(((all_masks['pos'].sum(axis=0)/k) >= 1).sum()))
+    print('{:.2f} neg edges passed the threshold at all folds: '.format(((all_masks['neg'].sum(axis=0)/k) >= 1).sum()))
     
     ## export pred table
-    fn = behav + '_' + corr_type + '_fold_' + str(k) + '_loocvpearson_pred.csv'
+    fn = behav + '_8yo_' + corr_type + '_fold_' + str(k) + '_loocvpearson_pred.csv'
     behav_obs_pred.to_csv(fn)
 
     ## plot pred vs obs
     g = plot_predictions(behav_obs_pred)
     g.figure.tight_layout()
-    plt.savefig(os.path.join('models', behav + '_' + corr_type + '_fold_' + str(k) + '_loocvpearson_model.png'))
+    plt.savefig(os.path.join('models', behav + '_8yo_' + corr_type + '_fold_' + str(k) + '_loocvpearson_model.png'))
     plt.close()
 
-    
+    ## save all_mask
+    np.save(fn + '_all_masks.npy',  all_masks)    
+
     ## edges
     for tail in all_masks.keys():
-        edfn = tail + '_' + behav + '_' + corr_type + '_fold_' + str(k)
+        edfn = tail + '_' + behav + '_8yo_' + corr_type + '_fold_' + str(k)
         
         edge_frac = (all_masks[tail].sum(axis=0))/(all_masks[tail].shape[0])
         edge_frac_square = sp.spatial.distance.squareform(edge_frac)
         
         # export pos and neg matrices
-        edmat = edfn + '_loocvpearson_bn.csv'
-        np.savetxt(edmat, edge_frac_square, delimiter=',', fmt='%1.0f')
+        # edmat = edfn + '_loocvpearson_bn.csv'
+        # np.savetxt(edmat, edge_frac_square, delimiter=',', fmt='%1.0f')
     
         # plot edges
         # all_masks(pos/neg matrices) is a binary array, k fold * n total edges, in this case is 10,69751
@@ -126,7 +135,7 @@ for behav in pca_data.columns[:2]:
         else:
             color = 'blue'
         
-        g = plot_consistent_edges(all_masks, tail, thresh = 1, color = color, node_coords = coords)
+        g = plot_consistent_edges(all_masks, tail, thresh = 0.8, color = color, node_coords = coords)
         plt.savefig(os.path.join('edges', edfn + '_loocvpearson_edges.png'))
         plt.close()
     
