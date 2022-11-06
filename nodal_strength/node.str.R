560947f5abbee5ef1a3a97bb97869aeec3dc0fcf
xscapintime <louiesxscape@gmail.com>
2022-11-06 19:57:49 +0800

    no need for nodla strength of fake dataset

--- nodal_strength/node.str.R
+++
@@ -1,9 +0,0 @@
-node.str=function(M) {
-  # check that input is square
-  if (dim(M)[1]!=dim(M)[1]) stop('the input matrix is not square')
-  n=dim(M)[1]
-  M[seq(from=1,to=n^2,by=(n+1))]=NaN
-  rowMeans(M,na.rm=T)
-}
-
-
