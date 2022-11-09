944a31f50b0ea155d6461cd5327d77fe0b97a5de
xscapintime <louiesxscape@gmail.com>
2022-11-09 10:23:41 +0800

    add spearman

--- cpm_iq_ptvsft/pt/checkvars.py
+++ cpm_iq_ptvsft/pt/checkvars.py
@@ -69,27 +69,6 @@ plt.savefig(os.path.join('dist', 'pt_8yo_wiscfull_fd_pairdist.png'))
 plt.close()
 
 
-
-
-## Check correlation between behavioral stat vs head motion
-# pearson
-for var in behav_data.columns[:7]:
-    
-    r = sp.stats.pearsonr(behav_data[var], behav_data['fd.m'])[0]
-    p = sp.stats.pearsonr(behav_data[var], behav_data['fd.m'])[1]
-
-    plt.figure(figsize = (5,5))
-    g = sns.regplot(x=behav_data[var], y=behav_data['fd.m'], color='blue')
-    g.figure.tight_layout()
-    g.annotate('r = {0:.2f}'.format(r), xy = (0.7, 0.1), xycoords = 'axes fraction')
-    g.annotate('p = {0:.2f}'.format(p), xy = (0.7, 0.05), xycoords = 'axes fraction') ##
-    plt.savefig(os.path.join('dist', var + '_vs_fd.png'))
-    plt.close()
-
-##### not correlated with motion much #####
-
-
-
 ## check normality of vars
 # Kolmogorov-Smirnov test  
 for var in behav_data.columns[:7]:
@@ -124,3 +103,50 @@ for var in behav_data.columns[:7]:
 # WISC_PS_CS: normally distributed. pval=0.1424320489168167
 # srs.rrb: NOT normally distributed. pval=7.27403359768175e-10
 # srs.sci: NOT normally distributed. pval=5.657304882333847e-06
+
+
+
+## Check correlation between behavioral stat vs head motion
+# pearson and spearman
+for var in behav_data.columns[:7]:
+
+    print(var)
+    print(sp.stats.pearsonr(behav_data[var], behav_data['fd.m']))
+    print(sp.stats.spearmanr(behav_data[var], behav_data['fd.m']))
+
+
+    # r = sp.stats.pearsonr(behav_data[var], behav_data['fd.m'])[0]
+    # p = sp.stats.pearsonr(behav_data[var], behav_data['fd.m'])[1]
+
+    # plt.figure(figsize = (5,5))
+    # g = sns.regplot(x=behav_data[var], y=behav_data['fd.m'], color='blue')
+    # g.figure.tight_layout()
+    # g.annotate('r = {0:.2f}'.format(r), xy = (0.7, 0.1), xycoords = 'axes fraction')
+    # g.annotate('p = {0:.2f}'.format(p), xy = (0.7, 0.05), xycoords = 'axes fraction') ##
+    # plt.savefig(os.path.join('dist', var + '_vs_fd.png'))
+    # plt.close()
+
+#####  #####
+
+
+# WISC_FULL_CS
+# (-0.25987592561851885, 0.014478648887248726)
+# SpearmanrResult(correlation=-0.2494823894163985, pvalue=0.019073827165565438)
+# WISC_VCI_CS
+# (-0.20963316396793588, 0.049965954830521665)
+# SpearmanrResult(correlation=-0.15596824177624358, pvalue=0.1467552258002097)
+# WISC_PR_CS
+# (-0.30249991649284874, 0.004175851587835593)
+# SpearmanrResult(correlation=-0.29852429114100243, pvalue=0.004726778680059869)
+# WISC_WM_CS
+# (-0.08811584081307375, 0.4142882303431613)
+# SpearmanrResult(correlation=-0.026869294467390475, pvalue=0.8037497277584218)
+# WISC_PS_CS
+# (-0.14146817251752836, 0.18859828875867224)
+# SpearmanrResult(correlation=-0.16383118923994244, pvalue=0.12720331013623595)
+# srs.rrb
+# (0.12227547338772883, 0.2564132326712549)
+# SpearmanrResult(correlation=0.08355653534931147, pvalue=0.43894426367078)
+# srs.sci
+# (0.14377381617291793, 0.18142238384489476)
+# SpearmanrResult(correlation=0.09670161417181553, pvalue=0.3701064839327348)
